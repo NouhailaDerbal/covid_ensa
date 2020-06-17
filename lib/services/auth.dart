d770fdb81350778bfce49d8d7acabbda4db3e670
omar atbd <omar.atbd@gmail.com>
2020-06-17 21:21:41 +0100

    service auth

---
+++ lib/services/auth.dart
@@ -0,0 +1,43 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:covidensa/models/user.dart';
+
+
+class AuthService {
+
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  //creation d'un user de type User a partir du user firebase
+  User _userFromFirebaseUser(FirebaseUser user){
+      return user != null ? User(uid: user.uid) : null;
+  }
+
+  //stream pour ecouter les changements au niveau stateUser
+  Stream<User> get user {
+    return _auth.onAuthStateChanged
+      .map(_userFromFirebaseUser);
+  }
+  //sign up avec un email et pass
+  Future register(String email, String password) async {
+    try {
+      AuthResult result = await _auth.createUserWithEmailAndPassword(email: email, password: password);
+      FirebaseUser user = result.user;
+      return _userFromFirebaseUser(user);
+    } catch(e) {
+      print(e.toString());
+      return null;
+    }
+  }
+
+  //sign in avec un email et pass
+
+
+  //sign out
+  Future signOut() async {
+    try {
+      return await _auth.signOut();
+    } catch(e) {
+      print(e.toString());
+      return null;
+    }
+  }
+}
