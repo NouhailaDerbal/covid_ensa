64fdf3fcd60292ff967b65a459978aed27bc8921
Soufiane Elhagani <soufianeelhaganie@gmail.com>
2020-06-27 00:22:20 +0100

    Stats

--- lib/pages/quest_page.dart
+++ lib/pages/quest_page.dart
@@ -1,4 +1,3 @@
-
 import 'package:covidensa/core/consts.dart';
 import 'package:covidensa/pages/home_page.dart';
 import 'package:covidensa/pages/question_page.dart';
@@ -6,6 +5,7 @@ import 'package:covidensa/pages/questions.dart';
 import 'package:covidensa/pages/map_page.dart';
 import 'package:covidensa/pages/formu_page.dart';
 import 'package:covidensa/pages/quiz_page.dart';
+import 'package:covidensa/pages/stat.dart';
 import 'package:covidensa/pages/statistic_page.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/foundation.dart';
@@ -18,9 +18,8 @@ import 'package:covidensa/services/database.dart';
 
 class QuestPage extends StatefulWidget {
   @override
-  _QuestPageState  createState() => _QuestPageState();
+  _QuestPageState createState() => _QuestPageState();
 }
-
 
 class _QuestPageState extends State<QuestPage> {
   //objet authService
@@ -30,102 +29,148 @@ class _QuestPageState extends State<QuestPage> {
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-             appBar: AppBar(
-               title: new Text("Questionnaire",),
-               elevation: defaultTargetPlatform==TargetPlatform.android?5.0:0.0,
-    ),
+      appBar: AppBar(
+        title: new Text(
+          "Questionnaire",
+        ),
+        elevation: defaultTargetPlatform == TargetPlatform.android ? 5.0 : 0.0,
+      ),
       drawer: Drawer(
         child: ListView(
           children: <Widget>[
-            new UserAccountsDrawerHeader(accountName: new Text("Genie Info"), accountEmail: new Text("ensas@gmailcom")
-             ,currentAccountPicture: new CircleAvatar(backgroundColor: Colors.deepPurpleAccent,child: new Text("GI"),),
-            otherAccountsPictures: <Widget>[
-              new CircleAvatar(backgroundColor: Colors.deepPurpleAccent,child: new Text("ND"),),
-
-
-            ],
+            new UserAccountsDrawerHeader(
+              accountName: new Text("Genie Info"),
+              accountEmail: new Text("ensas@gmailcom"),
+              currentAccountPicture: new CircleAvatar(
+                backgroundColor: Colors.deepPurpleAccent,
+                child: new Text("GI"),
+              ),
+              otherAccountsPictures: <Widget>[
+                new CircleAvatar(
+                  backgroundColor: Colors.deepPurpleAccent,
+                  child: new Text("ND"),
+                ),
+              ],
             ),
             new ListTile(
               title: new Text("Profil"),
               trailing: new Icon(Icons.person),
-               onTap:(){
-                 Navigator.of(context).push(MaterialPageRoute(builder:(_) => ForPage(), ),);
-               } ,
+              onTap: () {
+                Navigator.of(context).push(
+                  MaterialPageRoute(
+                    builder: (_) => ForPage(),
+                  ),
+                );
+              },
             ),
             new ListTile(
               title: new Text(" Questionaire"),
               trailing: new Icon(Icons.arrow_right),
-               onTap:(){
-                 Navigator.of(context).push(MaterialPageRoute(builder:(_) => QuestPage(), ),);
-               } ,
+              onTap: () {
+                Navigator.of(context).push(
+                  MaterialPageRoute(
+                    builder: (_) => QuestPage(),
+                  ),
+                );
+              },
             ),
             new ListTile(
               title: new Text("Ton etat "),
               trailing: new Icon(Icons.arrow_right),
-              onTap:() async {
+              onTap: () async {
                 dynamic result = await _db.tonEtat();
                 //print(result );
                 if (result == 'normal') {
-                  Navigator.of(context).push(MaterialPageRoute(builder:(_) => Eta1Page(), ),);
+                  Navigator.of(context).push(
+                    MaterialPageRoute(
+                      builder: (_) => Eta1Page(),
+                    ),
+                  );
                 } else if (result == 'ensuivi') {
-                  Navigator.of(context).push(MaterialPageRoute(builder:(_) => Eta2Page(), ),);
+                  Navigator.of(context).push(
+                    MaterialPageRoute(
+                      builder: (_) => Eta2Page(),
+                    ),
+                  );
                 } else if (result == 'infecté') {
-                  Navigator.of(context).push(MaterialPageRoute(builder:(_) => Eta3Page(), ),);
+                  Navigator.of(context).push(
+                    MaterialPageRoute(
+                      builder: (_) => Eta3Page(),
+                    ),
+                  );
                 }
-                
-              } ,
+              },
             ),
             new ListTile(
               title: new Text(" Localization"),
               trailing: new Icon(Icons.arrow_upward),
-              onTap:(){
-                Navigator.of(context).push(MaterialPageRoute(builder:(_) => MapePage(), ),);
-              } ,
+              onTap: () {
+                Navigator.of(context).push(
+                  MaterialPageRoute(
+                    builder: (_) => MapePage(),
+                  ),
+                );
+              },
             ),
             new ListTile(
               title: new Text("Statistiques"),
               trailing: new Icon(Icons.arrow_downward),
-              onTap:(){
-                Navigator.of(context).push(MaterialPageRoute(builder:(_) => StatisticPage(), ),);
-              } ,
+              onTap: () {
+                Navigator.of(context).push(
+                  MaterialPageRoute(
+                    builder: (_) => Stat(),
+                  ),
+                );
+              },
             ),
             new ListTile(
               title: new Text("Déconnexion"),
               trailing: new Icon(Icons.power),
-              onTap:() async{
+              onTap: () async {
                 await _auth.signOut();
-                Navigator.of(context).push(MaterialPageRoute(builder:(_) => HomePage(), ),);
-              } ,
+                Navigator.of(context).push(
+                  MaterialPageRoute(
+                    builder: (_) => HomePage(),
+                  ),
+                );
+              },
             ),
             new Divider(),
             new ListTile(
               title: new Text("Close"),
               trailing: new Icon(Icons.close),
-            onTap:()=> Navigator.of(context).pop(),
+              onTap: () => Navigator.of(context).pop(),
             )
           ],
         ),
       ),
       body: Container(
-      child: Column(
-
+          child: Column(
         children: <Widget>[
           Text("\n \n Testez-vous !!!\n ",
-              style: TextStyle(fontSize: 30 ,color: Colors.deepPurpleAccent,fontWeight: FontWeight.bold)),
-          Text(" Ce test vous aiderait à savoir si vous etes infectes par ce mortel virus\n ",
-              style: TextStyle(fontSize: 20,height:1.5,color: Colors.deepPurple),
+              style: TextStyle(
+                  fontSize: 30,
+                  color: Colors.deepPurpleAccent,
+                  fontWeight: FontWeight.bold)),
+          Text(
+              " Ce test vous aiderait à savoir si vous etes infectes par ce mortel virus\n ",
+              style: TextStyle(
+                  fontSize: 20, height: 1.5, color: Colors.deepPurple),
               textAlign: TextAlign.center),
           Image.asset('assets/images/quiz.png'),
           SizedBox(height: 25),
           GestureDetector(
-            onTap:(){
-              Navigator.of(context).push(MaterialPageRoute(builder:(_) => QuizPage(), ),);
-            } ,
-            child:Container(
+            onTap: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (_) => QuizPage(),
+                ),
+              );
+            },
+            child: Container(
                 decoration: BoxDecoration(
                     color: Colors.white,
-                    borderRadius: BorderRadius.all(
-                        Radius.circular(20)),
+                    borderRadius: BorderRadius.all(Radius.circular(20)),
                     boxShadow: [
                       BoxShadow(
                         color: Colors.black26,
@@ -133,20 +178,20 @@ class _QuestPageState extends State<QuestPage> {
                         spreadRadius: 3,
                         blurRadius: 3,
                       )
-                    ]
-                ),
+                    ]),
                 width: MediaQuery.of(context).size.width * .85,
                 height: 60,
-                child:Center(
-                    child:Text("GET STARTED",
-                      style:TextStyle(
-                          color: AppColors.mainColor,
-                          fontWeight: FontWeight.bold,
-                          fontSize: 20) ,))
-            ),),],
-      )
-      ),
-
+                child: Center(
+                    child: Text(
+                  "GET STARTED",
+                  style: TextStyle(
+                      color: AppColors.mainColor,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20),
+                ))),
+          ),
+        ],
+      )),
     );
-
-    }}
+  }
+}
